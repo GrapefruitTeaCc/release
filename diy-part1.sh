a5c36e84a82a20eafb1eacd58be5c7f1b3f42977
Name <you@example.com>
2022-04-12 21:48:21 +0800

    upgrade

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,10 +17,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-mkdir package/passwall
-pushd package/passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git
-popd
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall
 
 # luci-app-openclash
 mkdir package/luci-app-openclash
