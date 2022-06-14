a9b8d821ea9e81cc9bfcea610511b0a239a5854a
bXZb <89792508+bXZb@users.noreply.github.com>
2022-06-15 00:29:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,19 +15,19 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall
 
 # luci-app-openclash
-# mkdir package/luci-app-openclash
-# pushd package/luci-app-openclash
+mkdir package/luci-app-openclash
+pushd package/luci-app-openclash
 
-# git init
-# git remote add -f origin https://github.com/vernesong/OpenClash.git
-# git config core.sparsecheckout true
-# echo "luci-app-openclash" >> .git/info/sparse-checkout
-# git pull --depth 1 origin master
-# git branch --set-upstream-to=origin/master master
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
 
-# popd
+popd
